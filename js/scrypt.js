712e786741c42d9b535b67a63a5145770670004d
Yehor Kochniev <pro.realegor@gmail.com>
2024-05-22 12:43:36 +0300

    first commit

---
+++ js/scrypt.js
@@ -0,0 +1,6 @@
+function changeBackgroundImage()
+{
+    var header = document.getElementsByClassName('header');
+    header.style.backgroundImage = "url(../images/googlecalendar.png)";
+    header.style.backgroundSize = "contain";
+}
