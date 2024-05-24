a7af6f2cdd8256583f60de1500ea6c5733a5d882
Yehor Kochniev <pro.realegor@gmail.com>
2024-05-24 23:02:20 +0300

    Створив пасхалку(змінює весь стиль сторінки на чорний, якщо знайти спеціальну кнопку)

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,25 +1,65 @@
-var headers = document.getElementsByClassName('header');
-if(headers.length === 0) return;
+var navbar = document.getElementById("navbar");
+var navbar_list_links = document.getElementsByClassName("navbar-link");
+var main_text = document.getElementById("main-text");
+var main_section = document.getElementById("main-section");
+var header = document.getElementById("header");
+var footer = document.getElementById("footer");
+var change_theme_button = document.getElementById("change-theme-button");
 
-function changeSiteBackground() {    
-    
-    for(let i = 0; i < headers.length; i++){    
-        switch(headers[i].id) {
-            case 'index-header':
-                headers[i].style.backgroundImage = "linear-gradient(#1E497C, #8FBDBB, #f2f2f2)"
-                break;
-            case 'login-header':
-                headers[i].style.backgroundImage = "linear-gradient(#1E497C, #8FBDBB, #f2f2f2)"
-                break;
-            case 'obsidian-header':
-                headers[i].style.backgroundImage = "linear-gradient(#5125aa, #AA88EF, #f2f2f2)";
-                break;
-            case 'google-calendar-header':
-                headers[i].style.backgroundImage = "url(../images/googlecalendar.png)"
-                break;
-            default:  
+var theme = "light"
+
+
+function changeSiteTheme() {
+    // alert("Тема змінюється...");
+
+    if(theme === "light")
+    {
+        theme = "dark";
+        change_theme_button.style.backgroundColor = "black"
+        change_theme_button.style.borderColor = "white"
+
+        navbar.style.borderColor = "white";
+        navbar.style.backgroundColor = "black";
+        for(let i = 0; i < navbar_list_links.length; i++) {
+            var link = navbar_list_links[i];
+            link.style.color = "white";
         }
+        main_text.style.color = "white";
+        main_section.style.backgroundColor = "black";
+        header.style.backgroundColor = "#0d0d0d";
+        header.style.backgroundImage = "linear-gradient(#1E497C, #8FBDBB, #0d0d0d)";
+        footer.style.backgroundImage = "linear-gradient(black, white)";
+        
+    }
+    else if(theme === "dark")
+    {
+        theme = "light"
+        change_theme_button.style.backgroundColor = "white";
+        change_theme_button.style.borderColor = "black"
+
+        navbar.style.borderColor = "black";
+        navbar.style.backgroundColor = "white";
+        for(let i = 0; i < navbar_list_links.length; i++) {
+            var link = navbar_list_links[i];
+            link.style.color = "black";
+        }
+        main_text.style.color = "black";
+        main_section.style.backgroundColor = "white";
+        header.style.backgroundColor = "#f2f2f2";
+        header.style.backgroundImage = "linear-gradient(#1E497C, #8FBDBB, #f2f2f2)";
+        footer.style.backgroundImage = "linear-gradient(#8FBDBB, #1E497C)";
     }
 }
 
-changeSiteBackground();
+function setHoverEffect() {
+    change_theme_button.style.backgroundColor = "#808080";
+}
+
+function noHoverEffect() {
+    if(theme === "light") {
+        change_theme_button.style.backgroundColor = "white";
+    }
+    else if(theme === "dark") {
+        change_theme_button.style.backgroundColor = "black";
+    }
+}
